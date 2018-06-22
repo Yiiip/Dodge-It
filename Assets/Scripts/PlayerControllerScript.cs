0163e586532d34bdfdf24773cfdb0ced1e06ac97
Felix <984082450@qq.com>
2018-06-22 08:21:16 +0800

    Basic game control.

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody2D rigidbody;
+
+	private Vector2 mMoveVelocity;
+
+	private void Start()
+	{
+	}
+
+	private void Update()
+	{
+		Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		mMoveVelocity = moveInput.normalized * speed;
+	}
+
+	private void FixedUpdate() {
+		rigidbody.MovePosition(rigidbody.position + mMoveVelocity * Time.fixedDeltaTime);
+	}
+}
